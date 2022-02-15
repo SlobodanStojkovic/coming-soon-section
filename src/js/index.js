c39854558ed57a71383b24a8598aad40c0eaaa7a
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-02-15 20:16:26 +0100

    Change html, css, js code refactoring and improvements for diferent media sizes

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,43 @@
-let input = document.querySelector(".coming-soon-section__email__input");
-let button = document.querySelector(".coming-soon-section__email__button");
-let error = document.querySelector(".coming-soon-section__email__warning");
-let errorIcon = document.querySelector(".coming-soon-section__email__error");
+let submit = document.querySelector(".email-submit");
 
-const submitEmail = () => {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value)) {
-    console.log(input.value);
-    error.classList.add("hidden");
-    errorIcon.classList.add("hidden");
-    input.classList.remove("error");
-    return;
-  }
-
-  error.classList.remove("hidden");
-  errorIcon.classList.remove("hidden");
-  input.classList.remove("error");
-  input.classList.add("error");
+const messages = {
+  inputFieldEmpty: "Input field empty",
+  invalidEmail: "Please enter valid email address",
+  validationSuccessful: "You have subscribed successfully",
 };
 
-button.addEventListener("click", submitEmail);
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+};
+
+const submitEmail = () => {
+  let input = document.querySelector(".email-input");
+  let errorDiv = document.querySelector(".error-div");
+  let warningIcon = document.querySelector(".warning-icon");
+
+  if (input.value.length === 0) {
+    errorDiv.classList.remove("success");
+    errorDiv.innerHTML = messages.inputFieldEmpty;
+    input.classList.add("error");
+    warningIcon.classList.add("error");
+    console.log("empty");
+    return;
+  } else if (validateEmail(input.value) === null) {
+    errorDiv.classList.remove("success");
+    errorDiv.innerHTML = messages.invalidEmail;
+    input.classList.add("error");
+    warningIcon.classList.add("error");
+    console.log("invalid");
+    return;
+  } else if (validateEmail(input.value) !== null) {
+    errorDiv.innerHTML = messages.validationSuccessful;
+    errorDiv.classList.add("success");
+    input.classList.remove("error");
+    warningIcon.classList.remove("error");
+    return;
+  }
+};
+
+submit.addEventListener("click", submitEmail);
